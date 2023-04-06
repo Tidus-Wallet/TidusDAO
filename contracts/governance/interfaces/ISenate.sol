f76a559cb557cced3c5faa2f2f6d476dbfaa7bd7
sekaieth <sekaieth@proton.me>
2023-04-06 11:34:53 -0500

    update ISenate

--- contracts/governance/interfaces/ISenate.sol
+++ contracts/governance/interfaces/ISenate.sol
@@ -4,20 +4,29 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts-upgradeable/governance/IGovernorUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/extensions/IGovernorTimelockUpgradeable.sol";
 
-interface ISenate is IGovernorUpgradeable, IGovernorTimelockUpgradeable {
+abstract contract ISenate is IGovernorUpgradeable, IGovernorTimelockUpgradeable {
 
-    function vetoes(uint256 proposalId) external view returns (uint256 tribuneVetoes, uint256 consulVetoCount);
-    function positions(address account) external view returns (Position);
-    function governanceWhitelist(address account) external view returns (bool);
-    function senateVotingContract() external view returns (address);
+    enum Position {
+        None,
+        Consul,
+        Censor,
+        Tribune,
+        Senator,
+        Dictator
+    }
 
-    function updatePosition(address account, Position position) external;
-    function removePosition(address account) external;
-    function quorum(uint256 blockNumber) external pure returns (uint256);
-    function tribuneVeto(uint256 proposalId) external;
-    function consulVeto(uint256 proposalId) external;
-    function setPosition(address account, Position position) external;
+    function vetoes(uint256 proposalId) external virtual returns (uint256 tribuneVetoes, uint256 consulVetoCount);
+    function positions(address account) external virtual returns (Position);
+    function governanceWhitelist(address account) external virtual returns (bool);
+    function senateVotingContract() external virtual returns (address);
 
-    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description) external returns (uint256);
-    function proposalThreshold() external view returns (uint256);
+    function updatePosition(address account, Position position) external virtual;
+    function removePosition(address account) external virtual;
+    function quorum(uint256 blockNumber) public override virtual pure returns (uint256);
+    function tribuneVeto(uint256 proposalId) external virtual;
+    function consulVeto(uint256 proposalId) external virtual;
+    function setPosition(address account, Position position) external virtual;
+
+    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description) public override virtual returns (uint256);
+    function proposalThreshold() external virtual returns (uint256);
 }
