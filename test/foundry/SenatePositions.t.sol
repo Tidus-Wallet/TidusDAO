964d64ff279a67528da13e08f0a2e20095790793
sekaieth <sekaieth@proton.me>
2023-05-12 21:38:52 -0500

    set up mock senate & mint test for SenatePositions

--- test/foundry/SenatePositions.t.sol
+++ test/foundry/SenatePositions.t.sol
@@ -3,15 +3,24 @@ pragma solidity 0.8.18;
 import "forge-std/Test.sol";
 import { SenatePositions } from "../../contracts/ERC721/SenatePositions.sol";
 import { ISenatePositions } from "../../contracts/ERC721/interfaces/ISenatePositions.sol";
+import { Senate } from "../../contracts/governance/Senate.sol";
+import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
-contract MockSenate is Test {
+contract MockSenate is Test, ERC721Holder {
 
-	// Mock Senate contract
-	// This contract is used to test the SenatePositions contract
-	// It is used to mint tokens to the SenatePositions contract
-
-	ISenatePositions private senatePositions;
+	SenatePositions private senatePositions;
 	address constant SENATE_ADDRESS = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+	string[] metadatas = ["Consul", "Censor", "Tribune", "Senator", "Dictator"];
+	uint256[] termLengths = [1, 1, 1, 1, 1];
+
+	function setUp() public {
+		senatePositions = new SenatePositions(
+			address(this),
+			address(this),
+			metadatas,
+			termLengths
+		);
+	}
 
 	function test_mint() external {
 		senatePositions.mint(ISenatePositions.Position.Consul, address(this));
@@ -26,14 +35,15 @@ contract SenatePositionsTest is Test {
 
 	SenatePositions private senatePositions;
 
-	address senateContract = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+	Senate senateContract;
 	address timelockContract = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
 	string[] metadatas = ["Consul", "Censor", "Tribune", "Senator", "Dictator"];
 	uint256[] termLengths = [1, 1, 1, 1, 1];
 
 	function setUp() public {
+		senateContract = new Senate();
 		senatePositions = new SenatePositions(
-		senateContract,
+		address(senateContract),
 		timelockContract,
 		metadatas,
 		termLengths
